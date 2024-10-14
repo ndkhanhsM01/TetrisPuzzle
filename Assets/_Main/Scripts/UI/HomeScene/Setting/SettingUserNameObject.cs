d4b2f5f0d7a94d9572588cdc4ffae16815b526fb
ndkhanhs_m01 <142150794+ndkhanhsM01@users.noreply.github.com>
2024-10-14 21:48:57 +0700

    improve update name ui

--- Assets/_Main/Scripts/UI/HomeScene/Setting/SettingUserNameObject.cs
+++ Assets/_Main/Scripts/UI/HomeScene/Setting/SettingUserNameObject.cs
@@ -2,19 +2,44 @@
 
 using MLib;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows;
 
 public class SettingUserNameObject: MonoBehaviour
 {
     [SerializeField] private TMP_InputField inputName;
+    [SerializeField] private Button btnSubmit;
+
+    private void OnEnable()
+    {
+        btnSubmit.AddListener(OnClick_Submit);
+        inputName.onEndEdit.AddListener(UpdateUserName);
+    }
+
+    private void OnDisable()
+    {
+        btnSubmit.RemoveListener(OnClick_Submit);
+        inputName.onEndEdit.RemoveListener(UpdateUserName);
+    }
 
     public void ReadUserName()
     {
         inputName.text = DataManager.Instance.UserName;
     }
-    public void UpdateUserName()
+
+    private void OnClick_Submit()
     {
-        string input = inputName.text;
+        UpdateUserName(inputName.text);
+    }
+
+    private void UpdateUserName(string input)
+    {
+        //Debug.Log("Try update name: " + input);
+
+        if (input == DataManager.Instance.UserName) return;
+
         if (IsValidInputName(input))
         {
             DataManager.Instance.UserName = input;
